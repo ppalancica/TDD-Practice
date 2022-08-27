9fc8c9a7a021a925bba0bce3698c0a05cd82568d
System Administrator <root@Pavels-MacBook-Pro.local>
2022-08-28 02:01:55 +0300

    Added more tests into FeedMockDataService_Tests

--- SwiftUI-Unit-Testing/SwiftUI-Unit-Testing_Tests/FeedMockDataService_Tests.swift
+++ SwiftUI-Unit-Testing/SwiftUI-Unit-Testing_Tests/FeedMockDataService_Tests.swift
@@ -11,6 +11,8 @@ import Combine
 @testable import SwiftUI_Unit_Testing
 
 class FeedMockDataService_Tests: XCTestCase {
+    
+    var cancelables = Set<AnyCancellable>()
 
     override func setUpWithError() throws {
         // Put setup code here. This method is called before the invocation of each test method in the class.
@@ -18,6 +20,7 @@ class FeedMockDataService_Tests: XCTestCase {
 
     override func tearDownWithError() throws {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
+        cancelables.removeAll()
     }
     
     func test_FeedMockDataService_init_doesSetValuesCorrectly() {
@@ -35,5 +38,83 @@ class FeedMockDataService_Tests: XCTestCase {
         XCTAssertFalse(dataService1.items.isEmpty)
         XCTAssertTrue(dataService2.items.isEmpty)
         XCTAssertEqual(dataService3.items.count, items3?.count)
+    }
+    
+    func test_FeedMockDataService_downloadItemsWithEscaping_doesReturnValues() {
+        // Given
+        let dataService = FeedMockDataService(items: nil)
+        
+        // When
+        var items: [String] = []
+        let expectation = XCTestExpectation()
+        
+        dataService.downloadItemsWithEscaping { returnedItems in
+            items = returnedItems
+            expectation.fulfill()
+        }
+        
+        // Then
+        wait(for: [expectation], timeout: 5)
+        XCTAssertEqual(items.count, dataService.items.count)
+    }
+    
+    func test_FeedMockDataService_downloadItemsWithCombine_doesReturnValues() {
+        // Given
+        let dataService = FeedMockDataService(items: nil)
+        
+        // When
+        var items: [String] = []
+        let expectation = XCTestExpectation()
+        
+        dataService.downloadItemsWithCombine()
+            .sink { completion in
+                switch completion {
+                case .finished:
+                    expectation.fulfill()
+                case .failure:
+                    XCTFail()
+                }
+            } receiveValue: { returnedItems in
+                items = returnedItems
+            }
+            .store(in: &cancelables)
+        
+        // Then
+        wait(for: [expectation], timeout: 5)
+        XCTAssertEqual(items.count, dataService.items.count)
+    }
+    
+    func test_FeedMockDataService_downloadItemsWithCombine_doesFail() {
+        // Given
+        let dataService = FeedMockDataService(items: [])
+        
+        // When
+        var items: [String] = []
+        let expectation1 = XCTestExpectation(description: "Does throw an error")
+        let expectation2 = XCTestExpectation(description: "Does throw an error URLError.badServerResponse")
+        
+        dataService.downloadItemsWithCombine()
+            .sink { completion in
+                switch completion {
+                case .finished:
+                    XCTFail()
+                case .failure(let error):
+                    expectation1.fulfill()
+                    
+                    // let urlError = error as? URLError
+                    // XCTAssertEqual(urlError, URLError(.badServerResponse))
+                    
+                    if error as? URLError == URLError(.badServerResponse) {
+                        expectation2.fulfill()
+                    }
+                }
+            } receiveValue: { returnedItems in
+                items = returnedItems
+            }
+            .store(in: &cancelables)
+        
+        // Then
+        wait(for: [expectation1, expectation2], timeout: 5)
+        XCTAssertEqual(items.count, dataService.items.count)
     }
 }
